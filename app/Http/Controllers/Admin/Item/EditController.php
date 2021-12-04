98c871c6003ea578c73b20b5805f0221ca0c72c6
kazuyafudemae <naru.boru.gaoth@gmail.com>
2021-12-05 00:43:16 +0900

    namespace の修復

--- app/Http/Controllers/Admin/Item/EditController.php
+++ app/Http/Controllers/Admin/Item/EditController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Contrrollers\Admin\Item;
+namespace App\Http\Controllers\Admin\Item;
 
 use App\Item;
 use Illuminate\Http\Request;
--- app/Http/Controllers/Admin/Item/IndexController.php
+++ app/Http/Controllers/Admin/Item/IndexController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Contrrollers\Admin\Item;
+namespace App\Http\Controllers\Admin\Item;
 
+use App\Item;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
